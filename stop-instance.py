3535fe48a1a97c59a503fdbfc932010b705a041b
jgnunes <ferreiranunesjoao@gmail.com>
2017-05-22 17:12:04 -0300

    Update stop-instance.py

--- stop-instance.py
+++ stop-instance.py
@@ -1,15 +1,12 @@
-"""Codigo em Python que pára automaticamente uma instancia logo após
-   finalizar um comando."""
+"""Codigo em Python que pára (stop) automaticamente uma instancia 
+   logo após finalizar um comando. O input requerido é o comando
+   a ser executado. """
 
 import subprocess
 
-#Requer o comando a ser rodado
-comando = input("Cole aqui o comando a ser executado:")
-#Roda o comando
-subprocess.call(comando, shell=True)
-#Obtem o ID da instância atual
+comando = input("Cole aqui o comando a ser executado:")   #Solicita o comando a ser rodado
+subprocess.call(comando, shell=True)   #Roda o comando
 pipe = subprocess.Popen("wget -q -O - http://169.254.169.254/latest/meta-data/instance-id",
-                        shell=True, stdout=subprocess.PIPE).stdout
-output = pipe.read().decode("utf-8")
-#Pára a instância atual
-subprocess.call("aws ec2 stop-instances --instance-ids " + output, shell=True)
+                        shell=True, stdout=subprocess.PIPE).stdout   #Obtem o ID da instância atual
+output = pipe.read().decode("utf-8")   #Transforma byte (pipe) em string (output), com decodificacao utf-8
+subprocess.call("aws ec2 stop-instances --instance-ids " + output, shell=True)   #Pára a instância atual
